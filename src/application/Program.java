26c8bbec3116cd9969f236c13d48e6dc6a5ed9d4
Wesley Martins <wesleymartins.dev@outlook.com>
2023-07-18 19:52:52 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,5 +32,11 @@ public class Program {
 		for(Seller obj : list){
 			System.out.println(list);
 		}
+		
+		System.out.println("=== TEST 4: seller insert ===");
+		
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id =" + newSeller.getId());
 	}
 }
